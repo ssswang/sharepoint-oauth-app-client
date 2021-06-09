3f5be79597b171ee09743bd40a621f838b90d10c
ssswang <ssswang@users.noreply.github.com>
2021-06-09 17:13:40 -0400

    Update SPFile.php

--- src/SPFile.php
+++ src/SPFile.php
@@ -425,6 +425,52 @@ class SPFile extends SPObject implements SPItemInterface
         return new static($folder, $json, $extra);
     }
 
+        
+    public static function start(SPFolderInterface $folder, $data, $name = null, $guid)
+    {
+        $json = $folder->request("_api/web/getfilebyserverrelativeurl('".$name."')/startupload(uploadId='".$guid."')", [
+            'headers' => [
+                'Authorization'   => 'Bearer '.$folder->getSPAccessToken(),
+                'Accept'          => 'application/json',
+                'X-RequestDigest' => (string) $folder->getSPFormDigest(),
+                'Content-length'  => strlen($data),
+            ],
+            'body'    => $data,
+        ], 'POST');
+
+        return new static($folder, $json, []);
+    }
+
+    public static function continue(SPFolderInterface $folder, $data, $name = null, $guid, $offset)
+    {
+        $json = $folder->request("_api/web/getfilebyserverrelativeurl('".$name."')/continueupload(uploadId='".$guid."',fileOffset=".$offset.")", [
+            'headers' => [
+                'Authorization'   => 'Bearer '.$folder->getSPAccessToken(),
+                'Accept'          => 'application/json',
+                'X-RequestDigest' => (string) $folder->getSPFormDigest(),
+                'Content-length'  => strlen($data),
+            ],
+
+            'body'    => $data,
+        ], 'POST');
+        return new static($folder, $json, []);
+    }
+
+    public static function finish(SPFolderInterface $folder, $data, $name = null, $guid, $offset)
+    {
+        $json = $folder->request("_api/web/getfilebyserverrelativeurl('".$name."')/finishupload(uploadId='".$guid."',fileOffset=".$offset.")", [
+            'headers' => [
+                'Authorization'   => 'Bearer '.$folder->getSPAccessToken(),
+                'Accept'          => 'application/json',
+                'X-RequestDigest' => (string) $folder->getSPFormDigest(),
+                'Content-length'  => strlen($data),
+            ],
+
+            'body'    => $data,
+        ], 'POST');
+        return new static($folder, $json, []);
+    }
+    
     public static function createByTemplate($folder, $content, $name = null, $overwrite = false, $templateId, array $extra = [])
     {
         $folder->isWritable(true);
